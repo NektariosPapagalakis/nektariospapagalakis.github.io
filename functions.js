caa8a268731c780cccdfe095421b3d8eb28d5b57
DESKTOP-JDB71NO\nekta <nektarios00007@gmail.com>
2024-11-07 23:45:31 +0200

    fff

--- functions.js
+++ functions.js
@@ -1,26 +1,6 @@
-function change_image(element,img1,img2){
-  if (element.src.includes(img1)){
-    element.src = img2;
-  }
-  else{
-    element.src = img1;
-  }
-}
-window.onscroll = function(ev) {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight-47) {
-      document.getElementById("go_to_top_arrow").style.color ="#333";
-  }
-  else{
-    document.getElementById("go_to_top_arrow").style.color ="#3b8cd8";
-  }
-};
 
-function go_to_top(){
-  window.scrollTo({top:0,behavior:'smooth'})
-}
-
-function change_active_nav_element(element){
-  var active_element = document.getElementsByClassName("active");
-  active_element[0].classList.remove("active");
-  element.classList.add("active");
+function toggleText() {
+  const imgText = $('.img_text');
+  imgText.toggleClass('hide_img_text');
+  imgText.toggleClass('show_img_text');
 }
